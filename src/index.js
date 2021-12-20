1da1c5a3a8a9359f67a2f36cc6de4b24569c50fa
Tyrmundr <taki.96061@gmail.com>
2021-12-20 11:22:39 +0100

    Added comments and created website.js which'll include loading modules and compiling them

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 "use strict"
+
+//Initialize Page Function
---
+++ src/website.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// Imports to other modules
+
+// Creating a main page
+
+// Button calibration + click calibration = which page to load
+
+// Initializing the home page
