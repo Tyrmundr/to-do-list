7bde443f7c074beb4522d90144931d1b96f93fc3
Tyrmundr <taki.96061@gmail.com>
2022-01-03 10:29:39 +0100

    Played around with adding buttons to sidebar and home - task and project buttons

--- src/load-home.js
+++ src/load-home.js
@@ -6,18 +6,35 @@ function createSidebarMenu() {
     projectSidebar.classList.add("project-sidebar");
     projectSidebar.innerText = "Projects";
 
-    projectSidebar.appendChild(createAddBtn());
+    projectSidebar.appendChild(createAddProjectBtn());
 
     return projectSidebar;
 }
 
-function createProjectFolder() {
+function createProjectFolder(name) {
+    name = prompt("Name of your project folder?:");
+
+    const folder = document.createElement("div");
+    folder.classList.add("project-folder");
+
+    const projectName = document.createElement("p");
+    projectName.classList.add("project-folder-name");
+    projectName.innerText = name;
+
+    folder.appendChild(projectName);
+    folder.appendChild(createProjectContent());
     
+    return folder;
 }
 
-function createFolder(name) {
-    
+function createProjectContent() {
+    const projectContent = document.createElement("div");
+    projectContent.classList.add("project-folder-content");
+    projectContent.appendChild(createAddTaskBtn());
+
+    return projectContent;
 }
+
 
 function createHome() {
     const home = document.createElement("div");
@@ -28,15 +45,35 @@ function createHome() {
     message.innerText = "You have no tasks yet. Add a new one?"
 
     home.appendChild(message);
-    home.appendChild(createAddBtn());
+    home.appendChild(createAddTaskBtn());
 
     return home;
 }
 
-function createAddBtn() {
+function createAddTaskBtn() {
     const addBtn = document.createElement("button");
-    addBtn.classList.add("btn-side-add");
+    addBtn.classList.add("btn-task-add");
     addBtn.innerText = "+";
 
     return addBtn;
+}
+
+function createAddProjectBtn() {
+    const addBtn = document.createElement("button");
+    addBtn.classList.add("btn-project-add");
+    addBtn.innerText = "+";
+    addBtn.onclick(createProjectFolder());
+
+    return addBtn;
+}
+
+
+export default function loadHome() {
+    const main = document.getElementById("main");
+    main.innerText = "";
+    main.appendChild(createHome());
+
+    const sidebar = document.getElementById("sidebar");
+    sidebar.innerText = "";
+    sidebar.appendChild(createSidebarMenu())
 }
